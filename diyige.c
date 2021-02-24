5adf9cfe6d27e0c8c693638d8d48e6e5c0782f1a
Tom  Z <71478494+leigelaing@users.noreply.github.com>
2021-02-24 14:03:23 +0800

    Add files via upload

---
+++ diyige.c
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+/*
+//指针大小都是4个字节或者8个字节
+int  main()
+{
+	printf("%d\n", sizeof(char*));
+	printf("%d\n", sizeof(short*)); 
+	printf("%d\n", sizeof(int*));
+	printf("%d\n", sizeof(double*));
+	return 0;
+}
+
+*/
+
+/*
+int main()
+{
+	int a = 10;
+	int* p = &a;//指针变量
+	return 0;
+}
+*/
