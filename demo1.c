6d0e7462bfb3de70a2a5f2be4c4af5367bfc7b43
Tom  Z <71478494+leigelaing@users.noreply.github.com>
2021-03-13 08:02:57 +0800

    Add files via upload

--- demo1.c
+++ demo1.c
@@ -1,41 +1,213 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
 
 
 
 
 
 /*
+//题目描述
+//不使用累计乘法的基础上，通过位移运算（<<）实现2的n次方的计算
+//多组输入，每行输入整数n（0 <= n < 31）
 int main()
 {
-	//strerror——把错误码对应的错误信息的字符串地址返回
-  // printf("%s\n",strerror(errno));
-	//perror函数
-	FILE* pf = fopen("test2.txt", "r");
-	if (pf = NULL)
+	int n = 0;
+	while (scanf("%d", &n) != EOF)
 	{
-		perror("open file test2.txt");
-		//这个函数的好处在于，1.不用引入头文件，
-		//2.打印格式是  （open file test2.txt：  错误信息）这样看起来更加清晰      
-		
-		return 0;
+		printf("%d\n", 1 << n);  //将1右移几位就是几的2次方
 	}
-	//读文件
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+//题目描述
+//输入一行数字，出生日期包括  年月日中间没有分隔符
+//三行  第一行为出生年份，第二行为月份，第三行为日期，输出时如果月份或者天数为1为时补零 
+int main()
+{
+	int y = 0;
+	int m = 0;
+	int d = 0;
+	scanf("%4d %2d %2d", &y, &m, &d);
+	printf("%d\n", y);
+	printf("%02d\n", m);//"%02d\n"  不够用0填充  域宽为2 
+	printf("%02d\n", d);
+	return 0;
+}
+
+*/
+
+
+
+
+
+/*
+//转换以下ASCII码对应的字符并输出他们
+//73,32,99,97,110,32,100,111,32,105,116,33
+int main()
+{
+	char arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };
+	int i = 0;
+	for (i = 0; i < 12; i++)
+	{
+		printf("%c", arr[i]);
+	}
+	return 0;
+}
+
+*/
+
+
+
+
+/*
+//输入一个字符，该字符构成三角金字塔
+//例：1
+//     1 
+//    1 1
+//   1 1 1
+int main()
+{
+	//输入
+	char n = 0;
+	scanf("%c", &n);
+	//输出
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		//打印一行  先打印空格  再打印字符
+		int j = 0;
+		for (j = 0; j <5-1-i ; j++)
+		{
+			printf(" ");
+		}
+		//打印1
+		for (j = 0; j <= i; j++)
+		{
+			printf("%c ",n);//注意%d  后面有个空格
+		}
+		printf("\n");
+	}
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+/*
+//从键盘任意输入一个字符，判断是否为字母
+//多组输入，每行输入包括一个字符
+int main()
+{
 	int ch = 0;
-	while ((ch = fgetc(pf)) != EOF)
+	//getchar函数获取不到字符时  输入的是EOF（end  of file 值为 -1）  
+	while ((ch = getchar()) != EOF)
 	{
-		putchar(ch);
+		//判断字母
+		if ((ch >= "A"&&ch <= "Z") || (ch >= "a"&&ch <= "z"))
+		{
+			printf("yes\n");
+		}
+		else
+		{
+			printf("no\n");
+		}
+		//清理\n
+		getchar();
 	}
-	if (ferror(pf))
-	{
-		printf("error\n");
-	}
-	else if (feof(pf))
-	{
-		printf("end of file\n");
-	}
+	return 0;
+}
 
-	fclose(pf);
-	pf = NULL;
+*/
+
+
+
+
+
+/*
+//依次输入一个学生的学号，以及三科成绩
+//输出看代码
+int main()
+{
+	int num = 0;
+	float a = 0.0;
+	float b = 0.0;
+	float c = 0.0;
+	scanf("%d;%f, %f, %f ",&num,&a, &b, &c);
+	printf("%d;%.2f, %.2f, %.2f ", num, a, b, c);// %.2f输出结果为2位小数
+	return 0;
+}
+*/
+
+
+
+
+/*
+//printf函数有返回值，写程序输出Hello world  的返回值
+//printf函数返回值是整型，返回的是字符的个数
+int main()
+{
+	printf("%d", printf("%d", printf("%d", 43)));//结果是  4321
+
+
+	//方法二
+	printf("\n %d \n", printf("Hello world!"));
+
+	//方法一
+   int ret = printf("Hello world!");
+   printf("\n");
+   printf("%d\n", ret);
+   return 0;
+}
+
+*/
+
+
+
+
+/*
+//写一个十六进制数ABCDEF，输出十进制  所占域宽为15
+int main()
+{
+	//域宽 ：域宽就是前面有几个空格，2个域宽是一个空格
+	printf("%d\n", 15);
+	printf("%4d\n", 15);
+	printf("%15d \n", 0XABCDEF);//注意16进制数前面加0X
+	//printf可以使用格式控制串"%md" 输出域宽为m的十进制整数
+	return 0;
+}
+*/
+
+
+
+
+/*
+//sizeof—— 操作符
+int a = 10；
+sizeof（int）
+sizeof（a）
+
+int main()
+{
+
+	printf("The size of short is %d bytes.\n",sizeof(short));
+	printf("The size of int is %d bytes.\n", sizeof(int));
+	printf("The size of long is %d bytes.\n", sizeof(long));
+	printf("The size of long long is %d bytes.\n", sizeof(long long));
 	return 0;
 }
 
@@ -47,46 +219,14 @@ int main()
 /*
 int main()
 {
-	//EOF 
-	//feof();//EOF_end  of   file  ——文件结束标志
-	FILE* pf = fopen("test.txt", "r");
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	int ch = fgetc(pf);
-	printf("%d\n", ch);//-1
+//    \t   tab  水平制表符     \040 ——\ddd    d是8进制数字
+//以32作为ASCII码值代表的字符
 
+	printf(" v     v\n  v   v\n    v\n ");
 
-	fclose(pf);
-	pf = NULL;
-	return 0;
-}
-
-
-*/
-
-
-
-/*
-int  main()
-{
-	FILE* pf = fopen("test.txt", "r");
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	//1.定位文件指针
-	//fseek(pf,-2,SEEK_END);
-	int ch = fgetc(pf);
-	int pos = ftell(pf);
-	printf("%d\n", pos);
-	
-	rewind(pf);
-	 ch = fgetc(pf);
-	printf("%c\n", ch);
-	fcolse(pf);
-	pf = NULL;
+	printf("v     v\n");
+	printf("  v   v\n");
+	printf("    v\n");
 	return 0;
 }
 
@@ -96,37 +236,12 @@ int  main()
 
 
 /*
-struct S
-{
-	char name[20];
-	int age;
-	double score;
-};
+输出：practice makes perfect!
+//在线OJ方式刷题
+#include<stdio.h>
 int main()
 {
-	struct S s = { "张三", 20, 55.6 };
-	struct S tmp = { 0 };
-	//打开文件
-	FILE* pf = fopen("test.txt", "wb");
-	FILE* pf = fopen("test.txt", "rb");
-	//判断是否打开
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	//二进制的形式写文件
-	fwrite(&s, sizeof(struct S), 1, pf);
-	//翻译：将s结构体变量，大小为struct S大小空间  写一个，  写在pf指向的文件中
-	
-	//二进制的形式读文件
-	fread(&tmp, sizeof(struct S), 1, pf);
-	//翻译：将tmp结构体变量中，读入sizeof(struct S)大小的  一个结构体     从pf指向的文件中读出
-	
-	printf("%s %d %lf ", tmp.name, tmp.age, tmp.score);
-	fclose(pf);
-	pf = NULL;
+	printf("practice makes perfect!");
 	return 0;
 }
-
 */
-
--- demo2.c
+++ demo2.c
@@ -1,175 +1,39 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<errno.h>
 #include<string.h>
-
-/*
-
-1.scanf  /printf  是针对标准输入流/标准输出流格式化输入/输出语句
-2.fscanf/fprintf  是针对所有输入流/所有输出流格式化输入/输出语句
-3.  sscanf  是从字符串中读取格式化数据
-    sprintf   是吧格式化数据输成（存储到）字符串
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-struct S
-{
-	int n;
-	float score;
-	char arr[10];
-};
-int  main()
-{
-
-	struct S s = { 100,3.14f,"abcdef"};
-	struct S tmp = { 0 };
-	char buf[1024] = { 0 };
-	//把格式化的数据转换成字符串存储到buf
-	sprintf(buf, "%d %f %s ", s.n, s.score, s.arr);
-	printf("%s\n", buf);
-	//从buf中读取格式化的数据到tmp中
-	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);//arr 本身就是首元素地址
-	printf("%d %.2f %s", tmp.n, tmp.score, tmp.arr);
-	return 0;
-}
-*/
-/*
-struct S
-{
-	int n;
-	float score;
-	char arr[10];
-};
-int  main()
-{
-	struct S s = { 0 };
-	fscanf(stdin, "%d %f %s", &(s.n), &(s.score), s.arr);//arr 本身就是首元素地址
-	fprintf(stdout,"%d %.2f %s", s.n, s.score, s.arr);
-	return 0;
-}
-
-
-*/
-
-
-
-/*
-struct S
-{
-	int n;
-	float score;
-	char arr[10];
-};
-int  main()
-{
-	struct S s = { 100, 3.14f, "bit" };
-	FILE* pf = fppen("test.txt", "r");
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	//格式化的输入数据
-	fscanf(pf, "%d %f %s", &(s.n), &(s.score), s.arr);//arr 本身就是首元素地址
-	printf("%d %f %s", s.n, s.score, s.arr);
-
-	fclose(pf);
-	pf = NULL;
-	return 0;
-}
-
-*/
-
-
-
-/*
-struct S
-{
-	int n;
-	float score;
-	char arr[10];
-};
-int  main()
-{
-	struct S s = { 100, 3.14f, "bit" };
-	FILE* pf = fppen("test.txt", "w");
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	//格式化的形式写文件
-	fprintf(pf, "%d %f %s", s.n, s.score, s.arr);
-	fclose(pf);
-	pf = NULL;
-	return 0;
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
 int main()
 {
-	//从键盘读取一行文本文本信息
-	char buf[1024] = { 0 };
-	fgets(buf, 1024, stdin);//从标准输入读取
-	fputs(buf, stdout);//输出到标准输出流
-	gets(buf);
-	puts(buf);
 	
 	return 0;
 }
-*/
-
 
 
 
 
 /*
+
+5/2    == 2
+5.0/2.0   ==浮点型
+
+
+
+//输入一个字符 ，输出对应的ASCII值
 int main()
 {
-	char buf[1024] = { 0 };//定义一个数组
-	FILE* pf = fopen("test.txt", "w");
-	//打开失败需要判断
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	//写文件
-	fputs("hello\n", pf);
-	fputs("world\n", pf);
-	  
-	
-	//关闭文件
-	fclose(pf);
-	pf == NULL;
+	char a = 0;
+	//输入
+	scanf("%c", &a);
+	//输出
+	printf("%d\n", a);
+	return 0;
+}
+
+*/
+/*
+int main()
+{
+	int a = 0;
+	scanf("a = %d", &a);//输入时要注意 a =  ？？ 严格控制格式
 	return 0;
 }
 
@@ -179,73 +43,18 @@ int main()
 
 
 /*
-int main()
+int  main()
 {
-	char buf[1024] = { 0 };//定义一个数组
-	FILE* pf = fopen("test.txt", "r");
-	//打开失败需要判断
-	if (pf == NULL)
-	{
-		return 0;
-	}
-	//读文件
-	fgets(buf, 1024, pf);
-	printf("%s\n", buf);
-	fgets(buf, 1024, pf);
-	printf("%s\n", buf);
-//关闭文件
-	fclose(pf);
-	pf == NULL;
-	return 0;
-}
-*/
-
-
-
-
-/*
-int main()
-{
-	FILE* pf = fopen("test.txt", "r");//打开test.txt文件，并且以读的方式打开
-	//判断问题
-	if (pf == NULL)
-	{
-		printf("%s\n", strerror(errno));
-		return 0;
-	}
-	//读文件
-	int ch = fgetc(pf);
-	printf("%c", ch);
-    ch = fgetc(pf);
-	printf("%c", ch);
-    ch = fgetc(pf);
-	printf("%c", ch);
-	//关闭文件
-	fclose(pf);
-	pf = NULL;
-	return 0;
-}
-
-*/
-
-
-
-/*
-int main()
-{
-	FILE* pf = fopen("test.txt", "w");//打开文件，写入
-	if (pf == NULL)
-	{
-		printf("%s\n", strerror(errno));
-		return 0;
-	}
-	//写文件
-	fputc('b', pf);
-	fputc('i', pf);
-	fputc('t', pf);
-	//关闭文件
-	fclose(pf);
-	pf = NULL;
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	//交换
+	int c = 0;
+	c = a;
+	a = b;
+	b = c;
+	printf("a = %d\n",a);
+	printf("b = %d\n",b);
 	return 0;
 }
 */
