9acfa11e68252e7cdc11de0ad1b222660e42dec5
Tom  Z <71478494+leigelaing@users.noreply.github.com>
2021-03-23 09:14:54 +0800

    Add files via upload

--- demo1.c
+++ demo1.c
@@ -1,70 +1,123 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<limits.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-//计算一个数的每位之和（）递归实现
-//写一个函数 DigitSum（n）；输入一个非负数，返回组成它的数字之和
+int a;
 int main()
 {
+	int  a = 10;
+	int b  =  20;
+}
 
+
+
+
+
+
+/*
+//输入一个字符，第一确认 是否为小写字母，如果是 改为大写
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOf)
+	{
+		if (ch >= 'a' &&  ch <= 'z')
+		{
+			printf("%c\n", ch - 32);//将小写字母改为大写字母
+		}
+		else if (ch >= 'A' &&  ch <= 'B')
+		{
+			printf("%c\n", ch + 32);//大写转小写
+		}
+		getchar();
+	}
 	return 0;
 }
 
+*/
+
+
+
 
 
 
 
 /*
-//字符串逆序（递归实现）
-// 编写函数：reverse_string(char* string)(递归实现)
-int my_strlen(char* str)
-{
-	int count = 0;
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-	return count;
-}
-
-/*
-方法一
-void reverse_string(char arr[])
-{
-	int left = 0;
-	int right = my_strlen(arr) - 1;//找到最后一个元素
-	while (left < right)
-	{
-		int tmp = arr[left];
-		arr[left] = arr[right];
-		arr[right] = tmp;
-		left++;
-		right--;
-	}
-}
-
-
-//方法二：递归方法
-//这个代码没看懂
-void reverse_string(char arr[])
-{
-	char tmp = arr[0];//将arr[0]放入tmp
-	int len = my_strlen(arr);//计算arr数组元素个数，放入len
-	arr[0] = arr[len - 1];//第一个元素与最后一个元素交换
-		arr[len - 1] = '\0';//将最后一个元素放入 \0
-		if (my_strlen(arr + 1) >= 2)//
-			reverse_string(arr + 1);
-		arr[len - 1] = tmp;
-}
-
+//判断英文字母的辅音与元音
 int main()
 {
-	char arr[] = "abcdef";//fedcba
-	reverse_string(arr);
-	printf("%s\n", arr);
+	int ch = 0;
+	char s[] = "AaEeIiOoUu";
+	while ((ch = getchar()) != EOF)
+	{
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			if (ch = s[i])//靠for循环一个一个对比
+			{
+				printf("Vowel\n");
+				break;
+			}
+			if (i == 10)
+			{
+				printf("consonant\n");
+			}
+		}
+		//消除\n
+		getchar();
+	}
+	return 0;
+}
+
+*/
+
+
+
+
+/*
+int main()
+{
+	//EOF ——的值为 -1   全称为  End  of  file
+	//整型在计算机中存取的是补码   -1的补码取反是结果为 0
+	int score = 0;
+	while (~scanf("%d",&score))//等价于 scanf("%d",&score) != EOF  
+	{
+		if (score > 140)
+			print("你他娘，是个天才");
+	}
+	return 0;
+}
+
+*/
+
+
+
+
+
+/*
+//两名候选人A/B  社团没人只能投一票，得票多的是社长
+int main()
+{
+	int ch = 0;
+	int count_a = 0;
+	int count_b = 0;
+	while (((ch = getchar()) != '0') && ch != EOF)
+	{
+		if (ch = 'A')
+			count_a++;
+		if (ch = 'b')
+			count_b++;
+	}
+	if (count_a == count_b)
+	{
+		printf("%c\n", 'E');
+	}
+	if (count_a > count_b)
+	{
+		printf("%c\n", 'A');
+	}
+	if (count_a < count_b)
+	{
+		printf("%c\n", 'B');
+	}
 	return 0;
 }
 */
@@ -74,47 +127,100 @@ int main()
 
 
 
-
-
 /*
-//乘法表打印
-void print_table(int n)
-{
-	int i = 0;
-	for (i = 1; i <= n; i++)
-	{
-		//一行
-		int j = 0;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d * %d = %-3d", i, j, i*j);
-		}
-		printf("\n");
-	}
-}
+//两行数据  第一行输入一个数，表示n个学生（>=5）,第二行输入n个学生的成绩，（整数表示0-100之间）
 int main()
 {
 	int n = 0;
+	int arr[40] = { 0 };
+	//获取人数
 	scanf("%d", &n);
-	print_table(n);
-	return 0;
-}
-
-*/
-
-
-/*
-int main()
-{
-	double sun = 0;
 	int i = 0;
-	int flag = 1;
-	   for (i = 1; i <= 100; i++)
-	   {
-		   sun += flag*1.0 / i;
-		   flag = -flag;
-	   }
-	   printf("%lf\n", sun);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	//对n个学生的成绩排序——降序
+  //趟数
+	int i = 0;
+	for (i = 0; i < n - 1; i++)
+	{
+		//一趟冒泡排序
+		int j = 0;
+		for (j = 0; j < n - 1 - i; j++)//1 2 3 4 5
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				int tmp = 0;
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < 5; i++)//打印前五名的成绩
+	{
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
+*/
+
+
+
+
+
+
+/*
+//一件衣服，双十一  7七折， 双十二  八折   优惠卷减50 优惠卷只能在双十一或者双十二使用
+
+int main()
+{
+	float price = 0.0;
+	int m = 0;
+	int d = 0;
+	int flag = 0;
+	if (m == 11 && d == 11)
+	{
+		price *= 0.7;
+		if (flag = 1)
+		{
+			price -= 50;
+		}
+	}
+	if (m == 12 && d == 12)
+	{
+		price *= 0.8;
+		if (flag = 1)
+		{
+			price -= 50;
+		}
+	}
+	if (price < 0.0)
+	{
+		price = 0;
+	}
+	printf("%f\n", price);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+/*
+//输入一个十六进制的数，和一个8进制的数  求出a+b 的 结果 要为十进制
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%s %o", &a, &b);
+	int sum = a + b;
+	printf("%d\n", sum);
 	return 0;
 }
 
@@ -122,79 +228,107 @@ int main()
 
 
 
+
 /*
-//编写一段代码说明1-1000之间一共出现多少个九
+//从键盘输入5个人的成绩，求他们的平均成绩
 int main()
 {
 	int i = 0;
-	int count = 0;
-	for (i = 0; i <= 1000; i++)
+	int sum = 0;
+	for (i = 0; i < 5; i++)
 	{
-		if (i % 10 == 9)
-		{
-			count++;
-		}
-		if (i / 10 == 9)
-		{
-			count++;
-		}
-		if (i / 100 == 9)
-		{
-			count++;
-		}
+		int score = 0;
+		scanf("%d", &score);
+		sum += score;
 	}
-	printf("%d\n", count);
+	printf("%.lf\n", sum / 5.0);//保留一位小数
 	return 0;
 }
-
 */
 
 
-/*
-//判断一个数是否为素数  （相除法：2——i-1之间的数字相除）
-//方法二 ：i= a * b   a或者b中至少有一个数字 <= 开平方i
-int main()
-{
-	//aqrt ——开平方库函数
-	//
-	return 0;
-}
 
-*/
+
+
+
 
 /*
-//给两个数求最大公约数
-//辗转相除法
+//一箱牛奶里面有n盒未打开的牛奶，没h分钟喝光一盒，并且在喝光一盒之前不会再喝下一个。
+//那么经过m分钟还有多少盒没有打开
 
 int main()
 {
-	int a = 24;
-	int b = 18;
-	int r = 0;
-	scanf("%d %d", &a, &b);
-	while (a > b)
+	int n = 0;//牛奶数
+	int h = 0;//每分钟喝的数量
+	int m = 0;//一共多少分钟
+	if (m%h)//m%h 如果余数不为零 ，进入if  为零就不进 为什么？因为有余数意味着还要再拿一瓶来喝
 	{
-		 r = a % b;
-		if (r == 0)
-		{
-			printf("%d\n", b);
-			break;
-		}
-		a = b;
-		b = r;
+		printf("%d\n", n - m / h - 1);//减一的意思是一瓶还在喝着
 	}
-	while (b > a)
+	else
 	{
-		 r = b % a;
-		if (r == 0)
-		{
-			printf("%d\n", a);
-			break;
-		}
-		b = a;
-		a = r;
+		printf("%d\n", n - m / h);
 	}
-	
+	return 0;
+}
+*/
+
+
+/*
+//给定一个浮点数，求得该浮点数的个位
+int main()
+{
+	float n = 0;
+	scanf("%f ", &n);
+	printf("%d\n", ((int)n) % 10);//先将浮点数强制类型转换为int  然后取模就OK
+	return 0;
+}
+
+
+*/
+
+
+/*
+//变种水仙花数，把任意的数字 从中间拆分变为两个数字 比如 1461  可以拆分为1和461  14  61   146 1
+int main()
+{
+	int i = 0;
+	for (i = 10000; i <= 99999; i++)
+	{
+		int j = 0;
+		int sum = 0;
+		for (j = 10; j <= 10000; j *= 10)
+		{
+			sum += (i / j)*(i % j); //i/j算出的是商，i%j  算出的是余数
+		}
+		if (sum == i)
+		{
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
+
+*/
+
+
+
+/*
+//输入三科成绩  然后把成绩输出
+int main()
+{
+	int score1 = 0;
+	int score2 = 0;
+	int score3 = 0;
+	int score[3] = { 0 };
+	//输入
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		scanf("%d", &score[i]);
+	}
+   //输出
+	printf("score1 = %d score2 = %d  score3 = %d ", score1, score2, score3);
 	return 0;
 }
 */
@@ -202,39 +336,22 @@ int main()
 
 
 
-
 /*
+//构造三角形的三边  计算周长与面积   面积用海伦公式
 int main()
 {
 	int a = 0;
 	int b = 0;
 	int c = 0;
-	printf("请输入三个数字\n");
 	scanf("%d %d %d", &a, &b, &c);
-	if (a < b)
-	{
-		int tmp = 0;
-		tmp = a;
-		a = b;
-		b = tmp;
-	}
-	if (a < c)
-	{
-		int tmp = 0;
-		tmp = a;
-		a = c;
-		c = tmp;
-	}
-		
-	if (b < c)
-	{
-		int tmp = 0;
-		tmp = b;
-		b = c;
-		c = tmp;
-	}
-		
-	printf("%d %d %d", a, b, c);
+	//计算
+	double p = (a + b + c) / 2.0;//写2.0为了计算出double类型的数据
+	//周长
+	double circumference = a + b + c;
+	//sqrt是库函数 为了开平方
+	double area = sqrt(p*(p - a)*(p - b)*(p - c));
+	//输出
+	printf("circumference = %02lf  area = %02lf", circumference, area);
 	return 0;
 }
 */
@@ -243,112 +360,70 @@ int main()
 
 
 
-
 /*
-int sum(int a)
-{
-	int c = 0;
-	static int b = 3;//b为静态的局部变量
-	c += 1;
-	b += 2;
-	return(a + b + c);
-}
+//从键盘输入一个字符，编程判断这个字符是否为字母，（包括大小写）
+//多组输入
 int main()
 {
-	int i = 0;
-	int a = 2;
-	for (i = 0; i < 5; i++)
+	int ch = 0;
+	while (ch = getchar() != EOF)
 	{
-		printf("%d\n", sum(a));
+		//判断字母
+		if (ch >= 'A' && ch <= "Z" || ch >= 'A' && ch <= "Z")
+		{
+			printf("YES\n");
+		}
+		else
+		{
+			printf("NO\n");
+		}
+		getchar();//清除\n 为了下一次循环
 	}
 	return 0;
 }
-
 */
 
 
 
 
-
 /*
-//程序运行，你的电脑在一分钟后关机，如果输入 ：王李阳是傻逼，就不关 哈哈！
 int main()
 {
-	char  input[20] = { 0 };//存储数据
-	//关机
-	//在dos  窗口输入  shutdown -s -t 300
-	//在dos  窗口输入  shutdown -a    ——取消关机
-	//system()库函数  ——专门用来执行系统命令的
-	system("shutdown -s -t 300");//关机
-again:
-	printf("你的电脑在一分钟后关机，如果输入 ：王李阳是傻逼，就不关 哈哈！\n");
-	scanf("%s", input);//%s——字符串
-	if (strcmp(input, "王李阳是傻逼") == 0)//判断input中放的是不是  "王李阳是傻逼"  _strcmp_string compare
+	char password[20] = { 0 }；
+	printf("请输入密码>:");
+	scanf("%s", &password);//拿到密码后
+	//哪里来的那个\n  因为你输入密码完成后 会按下回车键
+	printf("请确认（Y/N）:>");
+	//密码要想正常，必须清楚\n
+	getchar();
+	int ch = getchar();//电脑接收到密码后，里面还有\n  还没等用户输入getchar读入的是\n  ,所以不会等用户输入
+	if (ch == 'Y')
 	{
-		system("shutdown -a");
+		printf("确认成功\n");
 	}
 	else
 	{
-		goto again;
+		printf("确认失败\n");
 	}
-		return 0;
+	return 0;
 }
-
 */
 
 
 
 
+
 /*
-//给定一个非空整型数组，除了某个元素只出现一次以外，其余元素每个元素均出现两次，找出那个只出现一次的元素
+//多组输入  每行输入大写字母，针对每组输入输出对应小写字母
 int  main()
 {
-	//3^3  结果为  0   自己去算  a^a = 0
-	//3^3^5 == 5
-	//3^5^3 == 5   异或满足交换律
-	//1^1^2^2^3^3^4^4^5 == 5
-	int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
-	//找出单身狗
-	int i = 0;
-	int ret = 0;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	for (i = 0; i < sz; i++)
+	//getchar 与scanf  从键盘输入先进入输入缓冲区  
+	int ch = 0;
+	//getchar  输入函数，每次那进来一个字符  而且还会拿进来\n  所以拿走的时候要清楚\n
+	while ((ch = getchar()) != EOF)
 	{
-		ret = ret^arr[i];//将所有元素全部异或，结果就是 答案
-	}
-
-	return 0;
-}
-*/
-
-
-
-
-
-/*
-int main()
-{
-	int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
-	//找出单身狗
-	int i = 0;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	for (i = 0; i < sz; i++)
-	{
-		//统计arr[i]在arr数组中出现过几次
-		int count = 0;//注意count的位置
-		int j = 0;
-		for (j = 0; j < sz; j++)
-		{
-			if (arr[i] == arr[j])
-			{
-				count++;
-			}
-		}
-		if (count == 1)
-		{
-			printf("  %d\n", arr[i]);
-			break;
-		}
+		putchar(ch + 32);
+		getchar();//清除 \n
 	}
 	return 0;
 }
@@ -357,42 +432,121 @@ int main()
 
 
 
+
 /*
+//必须掌握的格式控制字符
+/*
+  %c  字符
+  %hd  短整型
+  %d  整型
+  %s   字符串
+  %f 单精度浮点型
+  %lf  双精度浮点型
+  %p  地址格式
+
+
+//将四位数反向输出
 int main()
 {
-	INT_MAX;
-	//整型溢出
-	int a = 10;
-	int b = 20;
-	a = (a + b);
-	b = a - b;
-	a = a - b;
-
-	a = a^b; //按位（2进制位）异或  相同为0  相异为1
-	b = a^b;
-	a = a^b;
+	int n = 0;
+	//输入
+	scanf("%d", &n);
+	//输出
+	while (n != 0)
+	{
+		printf("%d", n % 10);
+		n = n / 10;
+	}
 
 	return 0;
-}
+} 
 
 */
 
 
 
 
+
 /*
+enum Sex
+{
+	MALE,
+	FEMALE,
+	SECRET
+};
+int main()
+{
+	enum Sex s = MALE;
+	return 0;
+}
+*/
 
 
-//给定秒数seconds（0<seconds<1000000000）把秒转换为小时，分钟
+/*
+//写代码，确认自身电脑是大端还是小端存储
+int check_sys1()
+{
+	int a = 1;
+
+	//00000000 00000000   00000000  00000001
+	//低地址  ——————   高地址
+	//返回  1  为低地址
+	//返回  0   为高地址
+
+	return *(char*)&a;//取a的地址，然后强制了类型转换为char* 的指针  意味着每次取出一个字节 
+	//1   如果取出的是  1  大端
+}
+
+int check_sys2()
+{
+	union Un
+	{
+		char c;
+		int i;
+	}u;
+	u.i = 1; //将1赋值为i
+	//联合体  共用4个字节
+	return u.c;
+	//返回  1  为低地址
+	//返回  0   为高地址
+}
 
 int main()
 {
-	int seconds = 0;
-	scanf("%d", &seconds);
-	int h = seconds / 60 / 60;//时
-	int m = seconds / 60 % 60;//分
-	int s = seconds % 60;//秒
-	printf("%d %d %d\n", h, m, s);
+	int a = 1;
+	int ret = check_sys1();
+	int ret = check_sys2();
+	if (1 == ret)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	return 0;
+}
+*/
+
+
+
+/*
+union un
+{
+	char c;//1
+	int i;//4
+	//一共5个字节   联合体是共用同一片空间  开辟空间使用较大的空间为准
+
+};
+int main()
+{
+	union un u;
+	printf("%d\n", sizeof(u));
+	
+	printf("%d\n", &u);
+
+	printf("%p\n", &(u.c));
+	printf("%p\n", &(u.i));
 	return 0;
 }
 */
--- demo2.c
+++ demo2.c
@@ -1,45 +1,20 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<limits.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-#include<time.h>
-/*
-typedef struct Stu
+int DigitSun(unsigned int num)
 {
-	//成员变量
-	char name[20];
-	short age;
-	char tele[12];
-	char sex[5];
-}Stu;
-//给struct Stu  重新命名为   Stu
-
-
-
-
-
-
-//struct 结构体关键字   Stu  ——结构体标签   struct Stu——结构体类型
-struct Stu
-{
-   //成员变量
-	char name[20];
-	short age;
-	char tele[12];
-	char sex[5];
-}s1,s2,s3;// s1,s2,s3全部为全局变量
-//创建类型不占用空间
+	if (num > 9)
+	{
+		return DigitSun(num / 10) + num % 10;
+	}
+}
 
 int main()
 {
-	struct Stu s;//创建变量才开辟空间    s为局部变量
-	struct Stu s1 = {"张三",20,"15814613413","男"};//结构体变量的初始化
-	return 0;
+	unsigned int num = 0;
+	scanf("%d", &num);
+	int ret = DigitSun(num);
+	printf("ret = %d\n", ret);
 }
-
-*/
 
 
 
@@ -47,160 +22,25 @@ int main()
 
 
 /*
-void print(int* p,int  sz)
-{
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d  ", *(p + i));
-	}
-
-}
+//A 想获取某年某月有多少天，输入年份和月份，计算这一年这个月有多少天
 int main()
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-  	print(arr, sz);
-	return 0;
-}
-
-
-*/
-
-
-/*
-//获取一个整数二进制序列中所有的偶数位和奇数位，分别打印出二进制序列
-void print(int n)
-{
-	int i = 0;
-	printf("奇数位\n");
-	for (i = 30; i >= 0; i -= 2)
-	{
-		printf("%d ", (n >> i) & 1);
-	}
-	printf("\n");
-	printf("偶数位\n");
-	for (i = 31; i >= 1; i-=2)
-	{
-		printf("%d ", (n >> i) & 1);
-	}
-	printf("\n");
-}
-
-int main()
-{
+	int y = 0;
 	int m = 0;
-	scanf("%d", &m);
-	print(m);
-	return 0;
-}
-
-*/
-
-
-
-
-
-/*
-//编程实现：两个int（32位）整数m和n的二进制表达中，有多少个二进制位不同
-
-int get_diff_bit(int m, int n)
-{
-	int tmp = m^n;//异或相同为0 不同为1
-	int count = 0;
-	while (tmp)
+	int days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	while (~scanf("%d %d", &y, &m))
 	{
-		tmp = tmp & (tmp - 1);
-		count++;
-	}
-	return count;
-}
-
-int main()
-{
-	int m = 0;
-	int n = 0;
-	scanf("%d %d", &m, &n);
-	int count = get_diff_bit(m, n);
-	printf("%d\n", count);
-	return 0;
-}
-
-*/
-
-
-
-
-
-/*
-int count_bit3(unsigned int n)
-{
-	//n = 13
-	//1101——13
-	//1100——12
-	//n = n & (n-1)
-	//1100  n = 12
-	//1011——11
-	//n = n & (n-1)
-	//1000—— 
-	//依次按位与
-	int count = 0;
-	while (n)
-	{
-		n = n & (n - 1);
-		count++;
-	}
-	return count;
-}
-
-
-
-
-
-//统计二进制中1 的个数
-//写一个函数求a的二进制（补码）表示有几个1
-int count_bit2(int n)
-{
-	int count = 0;
-	int i = 0;
-	for (i = 0; i < 32; i++)
-	{
-		if ((n >> i) & 1 == 1)//n的二进制位每一位与1进行按位与  结果等于1 确认n的二进制为1
+		int day = days[m - 1];
+		if((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
 		{
-			count++;
+			if (m == 2)
+			{
+				day++;
+			}
+			printf("%d\n", day);
 		}
+
 	}
-	return count;
-}
-
-
-
-
-int count_bit1(unsigned int n)//-1无法通过  所以改为unsigned int
-{
-	int count = 0;
-	while (n)
-	{
-		if (n % 2 == 1)
-		{
-			count++;
-		}
-		n = n / 2;
-	}
-	return count;
-}
-
-int main()
-{
-	int a = 0;
-	scanf("%d",&a);
-	int count1 = count_bit1(a);
-	int count2 = count_bit2(a);
-	int count3 = count_bit3(a);
-	printf("count1 = %d\n", count1);
-	printf("count2 = %d\n", count2);
-	printf("count3 = %d\n", count3);
 	return 0;
-	//system("pause");//system库函数   执行系统命令  pause（暂停）
 }
 */
