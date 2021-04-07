7c01adfa3336e50005a934c1305d57f5f3744e31
Tom  Z <71478494+leigelaing@users.noreply.github.com>
2021-04-08 07:46:31 +0800

    Add files via upload

--- demo1.c
+++ demo1.c
@@ -1,70 +1,171 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<limits.h>
-#include<stdlib.h>
-#include<string.h>
 #include<math.h>
-//计算一个数的每位之和（）递归实现
-//写一个函数 DigitSum（n）；输入一个非负数，返回组成它的数字之和
+struct student
+{
+	int num;
+	char name[32];
+	float  score;
+
+}stu;//stu 叫结构体变量名
+
+
+
+
+
+
+
+
+
+/*
+//打印菱形
+//   *
+//  ***
+// *****
+//  ***
+//   *
 int main()
 {
+	int line = 0;
+	scanf("%d", &line);
+	//打印上半部分
+	int i = 0;
+	for (i = 0; i < line; i++)
+	{
+		//打印空格
+		int j = 0;
+		for (j = 0; j < line - 1 - i; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		    printf("\n");
+	}
+	//打印下半部分
+	for (i = 0; i < line-1; i++)
+	{
+		//打印空格
+		int j = 0;
+		for (j = 0; j <=i ; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j <2*(line-1-i)-1 ; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+*/
 
+
+
+
+/*
+//水仙花数，求出0—100000之间的水仙花数
+//水仙花数指的是三位数
+
+
+//本代码求自幂数   
+
+int main()
+{
+	int i = 0;
+	for (i = 0; i <= 100000; i++)
+	{
+		//判断i是否为水仙花数（自幂数）
+		//1.计算i的位数—n位数
+		int n = 1;//数无论如何都是一位数  所以初始值为1
+		int tmp = i;
+		int sum = 0;
+		while (tmp /= 10)
+		{
+			n++;
+		       //i = i/10
+		}
+			//123/10   n++   商不为零 n++计数
+			//12/10   n++
+		//2.计算i的每一位的n次方之和  sum
+		tmp = i;
+		while (tmp)
+		{
+			sum += pow(tmp % 10, n);
+			tmp /= 10;
+		}
+		//3.比较i == sum
+		if (i == sum)
+		{
+			printf("%d ", i);
+		}
+	}
 	return 0;
 }
 
+*/
+
 
 
 
 
 /*
-//字符串逆序（递归实现）
-// 编写函数：reverse_string(char* string)(递归实现)
-int my_strlen(char* str)
+//求Sn = a+aa+aaa+aaaa+aaaaa的前五项之和，其中a为一个数字
+int main()
 {
-	int count = 0;
-	while (*str != '\0')
+	int a = 0;
+	int n = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &n);
+	int i = 0;
+	int ret = 0;
+	for (i = 0; i < n; i++)
 	{
-		count++;
-		str++;
+		ret = ret * 10 + 2;
+		sum = sum + ret;
 	}
-	return count;
+	printf("%d\n", sum);
+	return 0;
 }
+*/
+
+
+
 
 /*
-方法一
-void reverse_string(char arr[])
+#include<string.h>
+#include<assert.h>
+//写一个函数逆序字符串的内容
+void reverse(char* str)//str 指向  a
 {
-	int left = 0;
-	int right = my_strlen(arr) - 1;//找到最后一个元素
+	assert(str);//这个意思不清楚
+	int len = strlen(str);//使用shrlen函数求出字符串大小
+	char* left = str;//首元素地址
+	char* right = str + len - 1;//  最后一个元素的元素地址
 	while (left < right)
 	{
-		int tmp = arr[left];
-		arr[left] = arr[right];
-		arr[right] = tmp;
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
 		left++;
 		right--;
 	}
 }
-
-
-//方法二：递归方法
-//这个代码没看懂
-void reverse_string(char arr[])
-{
-	char tmp = arr[0];//将arr[0]放入tmp
-	int len = my_strlen(arr);//计算arr数组元素个数，放入len
-	arr[0] = arr[len - 1];//第一个元素与最后一个元素交换
-		arr[len - 1] = '\0';//将最后一个元素放入 \0
-		if (my_strlen(arr + 1) >= 2)//
-			reverse_string(arr + 1);
-		arr[len - 1] = tmp;
-}
-
 int main()
 {
-	char arr[] = "abcdef";//fedcba
-	reverse_string(arr);
-	printf("%s\n", arr);
+	char  arr[256] = { 0 };
+	//gets函数，读取一行内容
+	gets(arr);
+	//scanf函数遇见空格就不读取输入值了
+	//scanf("%s ", arr);//abcdef  ——fedcba
+	//逆序函数
+	reverse(arr);
+	printf("%s\n",arr);
 	return 0;
 }
 */
@@ -72,132 +173,28 @@ int main()
 
 
 
-
-
-
-
-/*
-//乘法表打印
-void print_table(int n)
-{
-	int i = 0;
-	for (i = 1; i <= n; i++)
-	{
-		//一行
-		int j = 0;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d * %d = %-3d", i, j, i*j);
-		}
-		printf("\n");
-	}
-}
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	print_table(n);
-	return 0;
-}
-
-*/
-
-
 /*
 int main()
 {
-	double sun = 0;
-	int i = 0;
-	int flag = 1;
-	   for (i = 1; i <= 100; i++)
-	   {
-		   sun += flag*1.0 / i;
-		   flag = -flag;
-	   }
-	   printf("%lf\n", sun);
-	return 0;
-}
-
-*/
-
-
-
-/*
-//编写一段代码说明1-1000之间一共出现多少个九
-int main()
-{
-	int i = 0;
-	int count = 0;
-	for (i = 0; i <= 1000; i++)
-	{
-		if (i % 10 == 9)
-		{
-			count++;
-		}
-		if (i / 10 == 9)
-		{
-			count++;
-		}
-		if (i / 100 == 9)
-		{
-			count++;
-		}
-	}
-	printf("%d\n", count);
-	return 0;
-}
-
-*/
-
-
-/*
-//判断一个数是否为素数  （相除法：2——i-1之间的数字相除）
-//方法二 ：i= a * b   a或者b中至少有一个数字 <= 开平方i
-int main()
-{
-	//aqrt ——开平方库函数
-	//
-	return 0;
-}
-
-*/
-
-/*
-//给两个数求最大公约数
-//辗转相除法
-
-int main()
-{
-	int a = 24;
-	int b = 18;
-	int r = 0;
-	scanf("%d %d", &a, &b);
-	while (a > b)
-	{
-		 r = a % b;
-		if (r == 0)
-		{
-			printf("%d\n", b);
-			break;
-		}
-		a = b;
-		b = r;
-	}
-	while (b > a)
-	{
-		 r = b % a;
-		if (r == 0)
-		{
-			printf("%d\n", a);
-			break;
-		}
-		b = a;
-		a = r;
-	}
+	int arr[10] = { 1, 2, 3, 4, 5 };
+	int* p = arr;//arr代表首元素地址
+	//*(p + 2) == p[2] == > *(arr + 2) == arr[2]
+	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	
+	int* ptr1 = (int*)(&aa + 1);
+	//&aa  是取出整个二维数组的地址，加1跳过整个二维数组，然后再将数组强制类型转换为int* 
+	int* ptr2 = (int*)(*(aa + 1));
+	//首先aa代表第一行的数组名，加1 等于跳过一整行，再将地址强制类型转换为int*型
+	printf("%d %d\n", *(ptr1 - 1), *(ptr2 - 1));
+	//*(ptr1 - 1)  等于将ptr1指向往回指向一个int型，结果是指向10；
+	//*(ptr2 - 1)  等于ptr2此时指向6，然后再减一指向5，最后结果就是5 
 	return 0;
 }
+
 */
+
+
+
 
 
 
@@ -206,193 +203,12 @@ int main()
 /*
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	printf("请输入三个数字\n");
-	scanf("%d %d %d", &a, &b, &c);
-	if (a < b)
-	{
-		int tmp = 0;
-		tmp = a;
-		a = b;
-		b = tmp;
-	}
-	if (a < c)
-	{
-		int tmp = 0;
-		tmp = a;
-		a = c;
-		c = tmp;
-	}
-		
-	if (b < c)
-	{
-		int tmp = 0;
-		tmp = b;
-		b = c;
-		c = tmp;
-	}
-		
-	printf("%d %d %d", a, b, c);
-	return 0;
-}
-*/
-
-
-
-
-
-
-/*
-int sum(int a)
-{
-	int c = 0;
-	static int b = 3;//b为静态的局部变量
-	c += 1;
-	b += 2;
-	return(a + b + c);
-}
-int main()
-{
-	int i = 0;
-	int a = 2;
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d\n", sum(a));
-	}
-	return 0;
-}
-
-*/
-
-
-
-
-
-/*
-//程序运行，你的电脑在一分钟后关机，如果输入 ：王李阳是傻逼，就不关 哈哈！
-int main()
-{
-	char  input[20] = { 0 };//存储数据
-	//关机
-	//在dos  窗口输入  shutdown -s -t 300
-	//在dos  窗口输入  shutdown -a    ——取消关机
-	//system()库函数  ——专门用来执行系统命令的
-	system("shutdown -s -t 300");//关机
-again:
-	printf("你的电脑在一分钟后关机，如果输入 ：王李阳是傻逼，就不关 哈哈！\n");
-	scanf("%s", input);//%s——字符串
-	if (strcmp(input, "王李阳是傻逼") == 0)//判断input中放的是不是  "王李阳是傻逼"  _strcmp_string compare
-	{
-		system("shutdown -a");
-	}
-	else
-	{
-		goto again;
-	}
-		return 0;
-}
-
-*/
-
-
-
-
-/*
-//给定一个非空整型数组，除了某个元素只出现一次以外，其余元素每个元素均出现两次，找出那个只出现一次的元素
-int  main()
-{
-	//3^3  结果为  0   自己去算  a^a = 0
-	//3^3^5 == 5
-	//3^5^3 == 5   异或满足交换律
-	//1^1^2^2^3^3^4^4^5 == 5
-	int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
-	//找出单身狗
-	int i = 0;
-	int ret = 0;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	for (i = 0; i < sz; i++)
-	{
-		ret = ret^arr[i];//将所有元素全部异或，结果就是 答案
-	}
-
-	return 0;
-}
-*/
-
-
-
-
-
-/*
-int main()
-{
-	int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
-	//找出单身狗
-	int i = 0;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	for (i = 0; i < sz; i++)
-	{
-		//统计arr[i]在arr数组中出现过几次
-		int count = 0;//注意count的位置
-		int j = 0;
-		for (j = 0; j < sz; j++)
-		{
-			if (arr[i] == arr[j])
-			{
-				count++;
-			}
-		}
-		if (count == 1)
-		{
-			printf("  %d\n", arr[i]);
-			break;
-		}
-	}
-	return 0;
-}
-
-*/
-
-
-
-/*
-int main()
-{
-	INT_MAX;
-	//整型溢出
-	int a = 10;
-	int b = 20;
-	a = (a + b);
-	b = a - b;
-	a = a - b;
-
-	a = a^b; //按位（2进制位）异或  相同为0  相异为1
-	b = a^b;
-	a = a^b;
+	int a[5][5];
+	int(*p)[4];
+	p = a;     //int(*)[4]—————————————int(*)[5]
+	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
 
 	return 0;
 }
 
-*/
-
-
-
-
-/*
-
-
-//给定秒数seconds（0<seconds<1000000000）把秒转换为小时，分钟
-
-int main()
-{
-	int seconds = 0;
-	scanf("%d", &seconds);
-	int h = seconds / 60 / 60;//时
-	int m = seconds / 60 % 60;//分
-	int s = seconds % 60;//秒
-	printf("%d %d %d\n", h, m, s);
-	return 0;
-}
 */
